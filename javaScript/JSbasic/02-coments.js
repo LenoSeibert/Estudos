76f9a48f0090cc1a81eb4928fb6ef264bc6786bd
Lenoir Seibert <siwebbr@gmail.com>
2021-08-24 11:14:10 -0300

    upd

--- javaScript/JSbasic/02-coments.js
+++ javaScript/JSbasic/02-coments.js
@@ -1,5 +1,13 @@
+//Comentário em Linha
+
+
 console.log("Olá Mundo") 
-console.log('Olá Mundo') 
-console.log('"Olá Mundo"') 
+console.log('Olá Mundo') //Comentário em Linha
+//console.log('"Olá Mundo"') Código Comentado
 console.log(`"'Olá Mundo'"`) 
 console.log('String', 10 , "Olá Mundo") 
+/**
+ * Bloco comentado
+ * mais uma linha 
+ * outra linha 
+ */
