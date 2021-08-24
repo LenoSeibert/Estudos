fe7f1670403eaf0965ab498a0528594a35f1dbcb
Lenoir Seibert <siwebbr@gmail.com>
2021-08-24 11:48:40 -0300

    upd

---
+++ javaScript/JSbasic/03-htmljs/script.js
@@ -0,0 +1,12 @@
+/**Variáveis 
+ * var foi descontinuado
+ * let 
+*/
+let nome1 //declara a variável
+nome1 = "Tio Leno"//define o valor
+console.log(nome1)//Exibe o valor da variável no console
+
+let nome = 'Lenoir Seibert' //declara e inicializa a variável
+console.log(nome)//exibe o valor armazenado na variável
+nome = 'John Doe' //Declara um novo valor para a Variável nome
+console.log(nome, 'tem',35 ,'anos e mora em SC')
