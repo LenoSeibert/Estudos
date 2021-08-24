320171a3489df3bf063c2ecec6da4e01d30b0848
Lenoir Seibert <siwebbr@gmail.com>
2021-08-24 12:23:11 -0300

    upd

--- javaScript/JSbasic/03-htmljs/script.js
+++ javaScript/JSbasic/03-htmljs/script.js
@@ -1,15 +1,20 @@
 //AS VARIÁVEIS DEVEM TER NOMES SIGNIFICATIVOS
 //PARA FACILITAR A LEITURA DO CÓDIGO
+///**Variáveis 
+//* var foi descontinuado
+//* let 
+//*/
+//let nome1 //declara a variável
+//nome1 = "Tio Leno"//define o valor
+//console.log(nome1)//Exibe o valor da variável no console
+//let nome = 'Lenoir Seibert' //declara e inicializa a variável
+//console.log(nome)//exibe o valor armazenado na variável
+//nome = 'John Doe' //Declara um novo valor para a Variável nome
+//console.log(nome, 'tem',35 ,'anos e mora em SC')
 
-/**Variáveis 
- * var foi descontinuado
- * let 
-*/
-let nome1 //declara a variável
-nome1 = "Tio Leno"//define o valor
-console.log(nome1)//Exibe o valor da variável no console
+//VARIÁVEIS CONSTANTES
 
-let nome = 'Lenoir Seibert' //declara e inicializa a variável
-console.log(nome)//exibe o valor armazenado na variável
-nome = 'John Doe' //Declara um novo valor para a Variável nome
-console.log(nome, 'tem',35 ,'anos e mora em SC')
+//const firstNum = 10
+//const secondNum = 5
+//const result = firstNum + secondNum 
+//console.log(result)
